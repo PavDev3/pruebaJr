d4a71d76d3b41d67e93822f17f0edf5fb9e07237
Pablo <pnunfe@gmail.com>
2023-10-23 14:53:57 +0200

    initialstate and handleSubmit and click

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,58 @@
 import './App.css'
+import { useState } from 'react'
 
+interface Item {
+  id: `${string}-${string}-${string}-${string}-${string}`,
+  timestamp: number,
+  text: string,
+}
+
+const INITIAL_ITEMS: Item[] = [
+  {
+    id: crypto.randomUUID(),
+    timestamp: Date.now(),
+    text: 'Elemento 1',
+  },
+  {
+    id: crypto.randomUUID(),
+    timestamp: Date.now(),
+    text: 'Elemento 2',
+  }
+]
 function App() {
- return (
+
+  const [items, setItems] = useState(INITIAL_ITEMS)
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(form)
+    const text = formData.get('item') as string
+    const newItem: Item = {
+      id: crypto.randomUUID(),
+      timestamp: Date.now(),
+      text,
+    }
+    setItems([...items, newItem])
+    form.reset()
+  }
+
+  const handleRemove = (id: string) => {
+    const newItems = items.filter((item) => item.id !== id)
+    setItems(newItems)
+  }
+
+
+  return (
   <main>
+    <header>
       <h1>Prueba técnica Jr</h1>
+    </header>
     <aside>
       <h4>Añadir y eliminar elementos de la lista</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
-          Elemento a introducir
+          Elemento a introducir:
           <input 
           name="item"
           required
@@ -20,11 +64,18 @@ function App() {
       </form>
     </aside>
     <section>
-      <h3>Lista de elementos</h3>
+      <h4>Lista de elementos</h4>
       <ul>
-        <li>Elemento 1</li>
-        <li>Elemento 2</li>
-        <li>Elemento 3</li>
+       {
+        items.map((item)  => {
+            return (
+            <li key={item.id}>
+              <span>{item.text}</span>
+              <button onClick={() => handleRemove(item.id)}>Eliminar</button>     
+            </li>
+          )}
+        )
+      }
       </ul>
     </section>
   </main>
