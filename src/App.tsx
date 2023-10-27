5fa3bf3a2082314a8b746404d7b1d77a84b4b339
Pablo <pnunfe@gmail.com>
2023-10-27 13:11:01 +0200

    comentarios en el codigo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { useEffect, useState } from 'react'
 
+
+// 1. Crear una lista de elementos
 interface Item {
   id: `${string}-${string}-${string}-${string}-${string}`,
   timestamp: number,
   text: string,
 }
-
+// 2. Añadir elementos a la lista de ejemplos
 const INITIAL_ITEMS: Item[] = [
   {
     id: crypto.randomUUID(),
@@ -21,9 +23,12 @@ const INITIAL_ITEMS: Item[] = [
 ]
 function App() {
 
+  // 3. Creamos un estado para la lista de elementos
   const [items, setItems] = useState(INITIAL_ITEMS)
+  // Creamos un estado para el botón de eliminar todos para que no se muestre si no hay elementos
   const [isButtonVisible, setIsButtonVisible] = useState(true)
 
+  // 4. Creamos un manejador de eventos para añadir elementos a la lista
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const form = event.currentTarget
@@ -38,20 +43,27 @@ function App() {
     form.reset()
   }
 
+  // 5. Creamos un handle para eliminar 1 elemento de la lista
   const handleRemove = (id: string) => {
     const newItems = items.filter((item) => item.id !== id)
     setItems(newItems)
   }
 
+  // 6. Creamos otro handle para eliminar todos los elementos de la lista
   const handleReset = () => {
     setItems([])
   }
 
+  // 7. Creamos un efecto para que el botón de eliminar todos no se muestre si no hay elementos
   useEffect(() => {
     setIsButtonVisible(items.length > 0)
   }, [items])
 
 
+  // Aquí ya mostramos la lista de elementos a la derecha y el formulario a la izquierda
+  // Tenemos un botón submit para añadir elementos a la lista
+  // También tenemos una section donde mostramos la lista de elementos siempre que haya alguno, si no, mostramos un mensaje
+  // Si la lista de elementos esta vacía, no mostramos el botón de eliminar todos
   return (
   <main>
     <header>
